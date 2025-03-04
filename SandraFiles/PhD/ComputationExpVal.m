0bb6c76d29801fb556af1cb36ee9ac521a6b8ded
Sandra Tomàs <sandratv14.7@gmail.com>
2025-03-04 17:36:18 +0100

    New ComputationExpVal
    With the missing operator error corrected I need to upload the functions to compute the expected values. Instead of one 1 I_if now we have 4 types and for each type (I^2, Ic^2, I*Ic, Is^2) i need to do the spin average over all the possible initial and final states.
    
    Also i included a code that directly writes .txt files with the results of the I_if depnendece on the M

--- SandraFiles/PhD/ComputationExpVal.m
+++ SandraFiles/PhD/ComputationExpVal.m
@@ -2,7 +2,7 @@
 setr0(3.964)
 %massa
 load("dades.mat","m_c","m_b")
-setm_q(m_b)
+setm_q(m_c)
 
 %values (autentic)
 Min=0.27;
@@ -16,51 +16,112 @@ k=50;
 %M row:
 nM=(Mfin-Min)/k;
 M=Min:nM:Mfin;
-Itheta=zeros(1,k+1);
 
-Itheta0=zeros(1,k+1);
-Itheta1=zeros(1,k+1);
-Itheta2=zeros(1,k+1);
-IthetaN=zeros(1,k+1); %N is negative =-1
-IthetaM=zeros(1,k+1); %M is dobule negative =-2
+%spin average de els termes involucrats enla integral
+I_if_square=zeros(1,k+1);
+I_if_c_square=zeros(1,k+1);
+I_if_0c_square=zeros(1,k+1);
+I_if_s_square=zeros(1,k+1);
+
+%directament les I_if sense square
+I_if0=zeros(1,k+1);
+I_if_c0=zeros(1,k+1);
+I_if1=zeros(1,k+1);
+I_if_c1=zeros(1,k+1);
+I_if_s1=zeros(1,k+1);
+I_if2=zeros(1,k+1);
+I_if_c2=zeros(1,k+1);
+I_if_s2=zeros(1,k+1);
 
 n=1;
 while n<=k+1
 %Inser here the transition following the guide of TransitionsAdded.m
-%
-transition0=I_thetaFunctions('HQsdtoP0');
-transition1=I_thetaFunctions('HQsdtoP1');
-transitionN=I_thetaFunctions('HQsdtoPn'); %n of negative
+%%%%%%%%%%%%%%%%%%%%%%%%%
 
-ExpValFunc=ExpValFunctions('HQ');
+%només 1 contribució Ji=0,m=0->Jf=0,m'=0
+%al spin average contribueixen els termes I^2, I_c^2 i I*I_c
 
-Itheta0(n)=ExpValFunc(3,2,M(n),transition0,1,1,true);
-Itheta1(n)=ExpValFunc(3,2,M(n),transition1,1,1,true);
-IthetaN(n)=ExpValFunc(3,2,M(n),transitionN,1,1,true);
+%canviar
+%estats inciials i finals
+Ni=n;
+Nf=n';
 
-%The N is not n in n(s/d)_J: remember the ordering of states for (s/d)1 and p1
+transition=FormFactor_ItoF('QQS0toS0_F/');
+transitionC=FormFactor_ItoF('QQS0toS0_Fc');
 
-Itheta(n)=( 2*(Itheta1(n)-IthetaN(n))*conj(Itheta1(n)-IthetaN(n)) )/3;
+ExpValFunc=ExpValFunctions('QQ');
 
+I_if(n)=ExpValFunc(Ni,Nf,M(n),transition,0,0);
+I_if_c(n)=ExpValFunc(Ni,Nf,M(n),transition_c,0,0);
 
-%%%%%
+%el spin average = com nomes hi ha 1 m possible per a la transicio
+%directament per a cada
+
+I_if_square(n)=I_if(n)^2;
+I_if_c_square(n)=I_if_c(n)^2;
+I_if_0c_square(n)=I_if(n)*I_if_c(n);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 fprintf('Number of iteration: %d / %d\n', n, k);
     n=n+1;
 
 end
 
-% Crear el scatter plot
-figure; % Crear una nueva figura
-scatter(M, Itheta, 60, 'filled'); % Scatter plot con puntos llenos y tamaño 100
 
-% Cambiar los nombres de los ejes
-xlabel('Dipion mass (GeV)');
-ylabel('I_{\theta}^2 (1/GeV)');
-
+%Aquetses variables es passen a fitxers notebook
 Mtrans=M';
-Itrans=Itheta';
+Itrans=I_if_square';
+Itrans_c=I_if_c_square';
+Itrans_0c=I_if_0c_square';
+Itrans_s=I_if_s_square';
 
- 
+% The following code is to create .txt files where the variables are stored
+baseName = 'QQcharm_3s-2s';
+folderPath = '/Users/sandra/Documents/Doctorat/Projectes\ PhD/Transicions\ a\ 2\ pions/Ordre\ més\ baix\ del\ Lagrangià/TransitionData';
+
+fileName = fullfile(folderPath, sprintf('%s.txt', baseName));
+fileName_c = fullfile(folderPath, sprintf('%s_c.txt', baseName));
+fileName_0c = fullfile(folderPath, sprintf('%s_0c.txt', baseName));
+fileName_s = fullfile(folderPath, sprintf('%s_s.txt', baseName));
+
+%Make sure the path exists
+if ~exist(folderPath, 'dir')
+    fprintf('Error: The folder "%s" does not exist.\n', folderPath);
+    return; 
+end
+
+% Open to write
+file = fopen(fileName, 'w');
+file_c = fopen(fileName_c, 'w');
+file_0c = fopen(fileName_0c, 'w');
+
+% Writhe the heading
+fprintf(file,    '%6s %12s\n', 'M', 'I_if^2');
+fprintf(file_c,  '%6s %12s\n', 'M', 'I_if_c^2');
+fprintf(file_0c, '%6s %12s\n', 'M', 'I_if*I_if_c');
+
+% Wirthe the two columns
+for i = 1:length(M)
+    fprintf(file,    '%.4f %12.8f\n', M(i), I_if_square(i));
+    fprintf(file_c,  '%.4f %12.8f\n', M(i), I_if_c_square(i));
+    fprintf(file_0c, '%.4f %12.8f\n', M(i), I_if_0c_square(i));
+end
+
+
+fclose(file);
+fclose(file_c);
+fclose(file_0c);
+
+%Not all transitions need a _s file so only created for thoes which need it
+%(p->p and d->s)
+if any(I_if_s_square)
+    file_s = fopen(fileName_s, 'w');
+    fprintf(file_s, '%6s %12s\n', 'M', 'I_if_s^2');
+    for i = 1:length(M)
+        fprintf(file_s, '%.4f %12.8f\n', M(i), I_if_s_square(i));
+    end
+    fclose(file_s);
+end
 
 
 
